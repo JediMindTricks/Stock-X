7dbf6c3ec4e9006ee0fb9ff1927f36310d1011a4
Michael <Michael.Bardwell.Scott@Gmail.com>
2018-05-24 18:11:09 -0400

    Setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Accordian from 'react-bootstrap';
+import Panel from 'react-bootstrap';
+import Button from 'react-bootstrap';
+import ButtonToolbar from 'react-bootstrap';
+import Modal from 'react-bootstrap';
+import FormGroup from 'react-bootstrap';
+import ControlLabel from 'react-bootstrap';
+import FormControl from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        
       </div>
     );
   }
